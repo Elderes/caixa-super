42220011a6c689171dc2e22dd3f1a910964aa7b6
Elderes <abraao.borges@academico.ifpb.edu.br>
2025-02-06 14:08:56 -0300

    init commit

---
+++ src/main/java/aps/caixa_super/CaixaSuperApplication.java
@@ -0,0 +1,13 @@
+package aps.caixa_super;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixaSuperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixaSuperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aps/caixa_super/CaixaSuperApplicationTests.java
@@ -0,0 +1,13 @@
+package aps.caixa_super;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaSuperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
